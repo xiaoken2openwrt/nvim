98010596a4c8279ea17ea079a1a0eb2897d0c61d
augus.sun <augus.sun@wtcy.com>
2026-03-15 15:19:47 +0800

    [gitsigns]完善快捷键映射

--- lua/plugins/git/gitsigns.lua
+++ lua/plugins/git/gitsigns.lua
@@ -104,6 +104,14 @@ return {
 
 				-- --- 文本对象 (Text object) ---
 				map({ "o", "x" }, "ih", gitsigns.select_hunk, "选择Git hunk")
+
+
+        map('n', '<leader>hs', gitsigns.stage_hunk)
+        map('n', '<leader>hr', gitsigns.reset_hunk)
+        map('v', '<leader>hs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+        map('v', '<leader>hr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+        map('n', '<leader>hS', gitsigns.stage_buffer)
+        map('n', '<leader>hR', gitsigns.reset_buffer)
 			end,
 		})
 	end,
